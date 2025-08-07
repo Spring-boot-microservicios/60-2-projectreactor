e60c8c418df47926addaebb6640212afb762b418
Luis Angel <angelfgdeveloper@gmail.com>
2025-08-06 22:25:21 -0600

    Uso de mono con next, success y error

--- src/main/java/org/angelfg/Main.java
+++ src/main/java/org/angelfg/Main.java
@@ -8,10 +8,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // Publisher
         Mono<String> mono = Mono.just("Hello world")
-                .doOnNext(value -> log.info("[onNext]: " + value));
+                .doOnNext(value -> log.info("[onNext]: " + value))
+                .doOnSuccess(value -> log.info("[onSucces]: " + value))
+                .doOnError(err -> log.info("[onError]: " + err.getMessage()));
 
-        mono.subscribe();
+        // Consumer
+        mono.subscribe(
+                data -> log.info("Receiving data: " + data),
+                err -> log.info("Error: " + err.getMessage()),
+                () -> log.info("Complete success")
+        );
+
+        // Consumer (Puedo usar tanto como los necesite)
+//        mono.subscribe(
+//                data -> log.info("Receiving data: " + data)
+//        );
 
     }
 
