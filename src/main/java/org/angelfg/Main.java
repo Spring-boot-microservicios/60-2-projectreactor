e46e9d782ac03a37e5a99cd03137c22e74d34ce0
Luis Angel <angelfgdeveloper@gmail.com>
2025-08-06 22:32:06 -0600

    Flux, next, complete y error

--- src/main/java/org/angelfg/Main.java
+++ src/main/java/org/angelfg/Main.java
@@ -1,13 +1,33 @@
 package org.angelfg;
 
 import lombok.extern.java.Log;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Log
 public class Main {
 
     public static void main(String[] args) {
+        // basesMono();
+        basesFlux();
+    }
 
+    private static void basesFlux() {
+        // Publisher
+        Flux<String> flux = Flux.just("Java", "Spring", "Reactor", "R2DBC")
+                .doOnNext(value -> log.info("[onNext]: " + value))
+                .doOnComplete(() -> log.info("[onComplete]: Success")) // Se ejecuta una unica vez, terminado todos los datos
+                .doOnError(err -> log.info("[onError]: " + err.getMessage()));
+
+        // Consumer
+        flux.subscribe(
+                data -> log.info("Receiving data: " + data),
+                err -> log.info("Error: " + err.getMessage()),
+                () -> log.info("Complete success")
+        );
+    }
+
+    private static void basesMono() {
         // Publisher
         Mono<String> mono = Mono.just("Hello world")
                 .doOnNext(value -> log.info("[onNext]: " + value))
@@ -25,7 +45,6 @@ public class Main {
 //        mono.subscribe(
 //                data -> log.info("Receiving data: " + data)
 //        );
-
     }
 
 }
